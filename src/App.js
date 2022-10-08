1a69944a74f7ff0e3c8ff932863e7d4fba82a624
ocean <osciris.veliz@gmail.com>
2022-10-08 16:45:24 -0500

    add name header; import google font

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div >
       {/* MAIN APP HERE */}
-      <h1>Hello world</h1>
+      <Header></Header>
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Header() {
+  return (
+    <div className='d-flex justify-content-between'>
+      <h1 className='mx-3'>Ocean</h1>
+    </div>
+  )
+}
+
+export default Header

c1ed3e7853f05c20944fa548cbe8c62ff0d1ce18
ocean <osciris.veliz@gmail.com>
2022-10-08 19:39:57 -0500

    add bootstrap styling and change css file back to App.css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import Header from './Header';
+import Header from './components/Header';
+import './App.css'
 
 function App() {
   return (
-    <div >
+    <div>
       {/* MAIN APP HERE */}
       <Header></Header>
     </div>
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header className='d-flex justify-content-between'>
+      <h1 className='mx-3'>
+        <a href='/'>Ocean</a>
+      </h1>
+    </header>
+  )
+}
+
+export default Header;

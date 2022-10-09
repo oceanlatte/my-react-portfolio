92f0edd827542c90a02dd6e2617626590539060d
ocean <osciris.veliz@gmail.com>
2022-10-09 17:24:12 -0500

    add back in the nav-links class that was removed in error; nav links styling finished

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import '../App.css';
 
 function Nav() {
   return (
-    <nav className='d-flex'>
+    <nav className='d-flex nav-links'>
      <a href='#about' className='px-3 nav-links'>About Me</a>
      <a href='#portfolio' className='px-3 nav-links'>Portfolio</a>
-     <a href='link' className='px-3 nav-links'>Contact</a>
+     <span className='px-3 nav-links' >Contact</span>
      <a href='link' className='px-3 nav-links'>Resume</a>
     </nav>
   )

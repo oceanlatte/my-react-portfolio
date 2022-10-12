7c04a8663f549b619774d30e53b1641caf09959d
ocean <osciris.veliz@gmail.com>
2022-10-11 20:55:18 -0500

    add form styling

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,23 +2,23 @@ import React from 'react';
 
 function Contact() {
   return (
-    <div>
+    <div className='ps-3 my-3'>
       <h2 className='section-title'>Contact ⌨️</h2>
       
-      <form >
-        <div>
-          <label htmlFor='name' >Name:</label>
-          <input type='text' name='name' />
+      <form className='col-11 col-xl-9'>
+        <div className='mb-3'>
+          <label htmlFor='name'>Name:</label>
+          <input type='text' name='name' className='form-control' />
         </div>
-        <div>
+        <div className='mb-3'>
           <label htmlFor='email'>Email</label>
-          <input type='email' name='email' />
+          <input type='email' name='email' className='form-control' />
         </div>
-        <div>
+        <div className='mb-3'>
           <label htmlFor='message'>Message</label>
-          <textarea name='message' rows='5' />
+          <textarea name='message' rows='5' className='form-control' />
         </div>
-        <button type='submit'>Submit</button>
+        <button type='submit' className='rounded-pill px-4 border-0 btn-style'>Submit</button>
       </form>
 
     </div>

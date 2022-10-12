ff7c8f4872b92c187852c85c777b79d1821de559
ocean <osciris.veliz@gmail.com>
2022-10-11 22:50:38 -0500

    finish styling of footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,20 +5,20 @@ import linkedInSvg from '../assets/images/linkedin-logo-duotone-svgrepo-com.svg'
 
 function Footer() {
   return (
-    <footer className='py-2 d-flex'>
+    <footer className='py-2 d-flex justify-content-center align-items-center'>
       Find me at: 
-      <ul className='ul-footer d-flex justify-content-end'>
-        <li>
+      <ul className='ul-footer d-flex'>
+        <li className='px-5'>
           <a href='https://twitter.com/oceanlatte222' target='_blank' rel='noreferrer'>
             <img src={twitterSvg} className='svg-container' alt='twitter logo'/>
           </a>
         </li>
-        <li>
+        <li className='px-5'>
           <a href='https://github.com/oceanlatte' target='_blank' rel='noreferrer'>
             <img src={gitHubSvg} className='svg-container' alt='gitHub logo'/>
           </a>
         </li>
-        <li>
+        <li className='px-5'>
           <a href='https://www.linkedin.com/in/ocean-veliz-921693239/' target='_blank' rel='noreferrer'>
             <img src={linkedInSvg} className='svg-container' alt='linkedIn logo'/>
           </a>

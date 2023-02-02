d8f1f7ae71a1dade4b0b2c6a38b745acc7c1b756
ocean <osciris.veliz@gmail.com>
2023-02-01 21:36:50 -0600

    add state hook to href, handleClick func takes clicked name & takes user to correct anchor link

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,39 +2,36 @@ import React, { useState } from 'react';
 import '../App.css';
 
 function Nav() {
-  const [currentPage, setCurrentPage]  = useState('');
-  // const [clickedLink, setClickedLink] = useState(true);
-
-  // Handles getting a single word link 
+  const [currentPage, setCurrentPage]  = useState('/');
+  
+  // Handles getting the name of the clicked Navbar link 
   function handleClick({ target }) {
     const clickedHeader = target.outerText;
     const pageName = clickedHeader.toLowerCase().trim();
 
-    // if the clicked nav bar name is more than two words, replace the space with a '-' dash
+    // if clicked navbar name is more than two words, replace the space with a dash
     if (pageName.search(' ')) {
       const newLinkName = pageName.replace(' ', '-');
-      console.log(newLinkName);
-      // setCurrentPage(newLinkName);
+      const newHrefName = '#' + newLinkName
+      setCurrentPage(newHrefName);
+    } else {
+      setCurrentPage('#' + pageName)
     }
-    // setCurrentPage(pageName);
-    // console.log(currentPage, 'current page val')
+
   }
 
-  // const handleClick = ({ target }) => setCurrentPage(target.innerHTML)
-  
-
   return (
-    <nav className='d-flex nav-links' onClick={handleClick}>
-      <a href='#about-me' className='px-3 nav-links' >
+    <nav className='d-flex nav-links' >
+      <a href={currentPage} className='px-3 nav-links' onClick={handleClick} >
           About Me
         </a>
-      <a href='#portfolio' className='px-3 nav-links'>
+      <a href={currentPage} className='px-3 nav-links' onClick={handleClick}>
           Portfolio
         </a>
-      <a href='#contact' className='px-3 nav-links' >
+      <a href={currentPage} className='px-3 nav-links' onClick={handleClick}>
           Contact
         </a>
-      <a href='#resume' className='px-3 nav-links'>
+      <a href={currentPage} className='px-3 nav-links' onClick={handleClick}>
           Resume
         </a>
     </nav>

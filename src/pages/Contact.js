97e763d3fb5de2e9823472ea22c514a99c5e53c6
ocean <osciris.veliz@gmail.com>
2023-02-12 15:07:46 -0600

    add icons to contact page, rename icon file names. Needs styling

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,15 +1,33 @@
 import React from 'react';
-// import { emailValidate } from '../utils/emailValidate';
+import phoneIcon from '../assets/images/contact-icons/mobile-svgrepo.svg'
+import emailIcon from '../assets/images/contact-icons/envelope-svgrepo.svg'
+import commentIcon from '../assets/images/contact-icons/comment-svgrepo.svg'
+import linkedInIcon from '../assets/images/contact-icons/linkedin-svgrepo.svg'
 
 function Contact() {
+  // lightbulb for name difference
+  // grid of phone, email, linkedin ...twitter? github?
   
   return (
     <div className='ps-3 my-3' id='contact'>
       <h2 className='section-title'>Contact</h2>
-        <h3>Phone:</h3>
-        <h3>Email:</h3>
-        <h3>LinkedIn:</h3>
-        <h3>GitHub:</h3>
+        <div>
+          <img src={commentIcon}/>
+          <ul className='d-flex flex-wrap'>
+            <li>
+              <img src={phoneIcon}/>
+              <p>(608) 852-2565</p>
+            </li>
+            <li>
+              <img src={emailIcon}/>
+              <p>osciris.veliz@gmail.com</p>
+            </li>
+            <li>
+              <img src={linkedInIcon}/>
+              <p>linkedIn</p>
+            </li>
+          </ul>
+        </div>
     </div>
   )
 };

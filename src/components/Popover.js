649c1c05f9f6a900f7cfc75a584806d1b16ca020
ocean <osciris.veliz@gmail.com>
2023-02-18 23:13:41 -0600

    get hover functionality to question icon working without reac-popper

--- src/components/Popover.js
+++ src/components/Popover.js
@@ -7,16 +7,40 @@ function Popover () {
   const [referenceElement, setReferenceElement] = useState(null);
   const [popperElement, setPopperElement] = useState(null);
   const [arrowElement, setArrowElement] = useState(null);
+  const [showMessage, setShowMessage] = useState(false);
+  const visibleEl = document.getElementById('popover')
+
+  const { styles, attributes } = usePopper(
+    referenceElement, popperElement, {
+    modifiers: [{ 
+      name: 'arrow', options: { element: arrowElement } 
+    }],
+  });
+
+  const handleMouseOver = () => {
+    visibleEl.setAttribute('style', 'visibility:visible');
+    setShowMessage(true);
+  }
+
+  const handleMouseLeave = () => {
+    visibleEl.setAttribute('style', 'visibility:hidden');
+    setShowMessage(false);
+  }
 
 return (
-  <div>
+  <div className='d-flex'>
     {/* Button */}
-    <button>
-      <img src={commentIcon} alt='chat bubble icon'className='comment-container' />
-    </button>
+      <img 
+        src={commentIcon} 
+        alt='chat bubble icon'
+        className='comment-container' 
+        onMouseOver={handleMouseOver} 
+        onMouseLeave={handleMouseLeave}
+      />
     {/* Tooltip  */}
-    <div id='tooltip' role='tooltip'>
-      My tooltip popover
+    <div ref={setPopperElement} style={styles.popper} {...attributes.popper} id='popover'>
+        Popper element
+        <div ref={setArrowElement} style={styles.arrow} />
     </div>
   </div>
 

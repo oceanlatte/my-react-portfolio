31c3af6b33c109902b755f3becb94541978983a3
ocean <osciris.veliz@gmail.com>
2023-08-15 20:07:35 -0500

    add pronouns to About Me; Change color of Nav links

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,7 +5,7 @@ function About() {
   return (
     <article id='about-me'>
       <h2 className='section-title text-center mb-3 mt-5'>
-        Hello!👋🏽 My name is <span className="about-title-span">Ocean</span>.
+        Hello!👋🏽 My name is <span className="about-title-span">Ocean</span>.<span id='pronoun-span'> (she/her)</span>
       </h2>
       <div className='d-flex align-items-center justify-content-between'>
         <img src={selfie} style={{ width: "250px" }} alt='selfie of app author' className='avatar rounded-circle ps-3'/>
@@ -18,7 +18,7 @@ function About() {
         I have been in the workforce since the age of 14 and graduated high school at 16.
         I have 12 years of experience in a team setting.
         I'm known for being a top performer and a leader in my roles. I am a fast learning problem solver looking for a team willing
-        to work with me on my career development as a web developer. 
+        to work with me on my career as a web developer. 
         <br/> 
         <br/>
         <span className="fun-fact-title">Fun fact: </span> 
